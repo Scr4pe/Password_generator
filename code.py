754160740335d7743b729601f76af20391f2f4fd
Scr4pe <83241458+Scr4pe@users.noreply.github.com>
2021-05-02 02:42:19 +0200

    Before Finishline

--- code.py
+++ code.py
@@ -1,30 +1,29 @@
 # Import Modules
-import strgen
 import string
 import random
 
 # Variables
 length_of_password = 0
 kind_of_password = 0
-Start_text = "\n\nWelcome to the Password generator\n"
-Options_text = "\nChoose which kind of password you want to create:\n1. Letters, Punctuations, Numbers\n2. Letters, punctuation\n3. Numbers, punctuation\n4. Letters, Numbers\n5. Only Numbers\n6. Only Letters\n7. Only punctuation\n"
+Start_text = "\n\nWelcome to the Password generator\nWe would advice you to use 14 to 22 Characters"
+Options_text = "\nChoose which kind of password you want to create:\n1. Letters, Punctuations, Numbers\n2. Hexadecimal\n3. Letters, Punctuation\n4. Numbers, Punctuation\n5. Letters, Numbers\n"
 length_of_password_text = "\nChoose the length of your Password"
 User_input_invalid = "The user selected an invalid value\n"
 user_selected = ""
-password_length_short ="Your Password is to short below 14 characters"
-password_length_long = "Your Password is to long above 128 characters\nThis Application doesn't support so long characters\n"
-password_length_change = "\nDo you want to change the length below?\n[Y/N] "
-password_length_remain = "Password legth remains as you selected\n"
+password_length_short ="\nYour Password is to short, below 14 characters"
+password_length_long = "\nYour Password is to long, above 22 characters\nThis Application doesn't support so long characters\n"
+password_length_change = "Do you want to change the length below?\n[Y/N] "
+password_length_remain = "Password length remains as you selected\n"
 password = ""
-
-# Text thank you for using our services (After password was printed out)
+password_length_new = "\nSelect new Password length"
 output_after_completion = "\nThank you for using our service\n"
 
-
 # Functions
+
+# This function is for Dealing with invalid input
 def invalid_value():
     print(User_input_invalid)
-    user_selected = input("Do you want to retry?\n[Y/N]")
+    invalid_value.user_selected = input("Do you want to retry?\n[Y/N]")
 
 # This function is for checking if the length of the password is valid
 def password_length():
@@ -36,7 +35,7 @@ def password_length():
         user_selected = input(password_length_change)
         # True if you want to change the Password length
         if (user_selected == "Y" or user_selected == "y"):
-            print(password_length_change)
+            print(password_length_new)
             password_length()
         # True if you want to continue with length
         elif(user_selected == "N" or user_selected == "n"):
@@ -45,13 +44,13 @@ def password_length():
         else:
             print(User_input_invalid)
             password_length()
-    # If your password is longer then 128 letters/numbers/symbols
-    elif (password_length.length_of_password > 128):
+    # If your password is longer then 22 letters/numbers/symbols
+    if (password_length.length_of_password > 22):
         # Ask the user if a shorter password is a better idea
         print(password_length_long)
         user_selected = input(password_length_change)
         if (user_selected == "Y" or user_selected == "y"):
-            print(password_length_change)
+            print(password_length_new)
             password_length()
         elif (user_selected == "N" or user_selected == "n"):
             print(password_length_remain)
@@ -61,73 +60,52 @@ def password_length():
 
 # This function creates the password
 def password_kind_input():
+    # Print instructions for the user
     print(Options_text)
-    # Decision 1 which kind of PSW
-    Kind_of_password = int(input())
+    password_kind_input.Kind_of_password = input()
 
     # If selection is letters, punctuations, numbers
-    if (Kind_of_password == 1):
+    if (password_kind_input.Kind_of_password == "1"):
         # Generate a password in which there are 
         # no repeating letters (lowercase or uppercase) and numbers and punctations
         # And cutted it to the selected length 
         password_kind_input.password = "".join(random.sample((string.ascii_uppercase + string.ascii_lowercase + string.punctuation + string.digits),password_length.length_of_password))
 
-    # If selection is hexadecimal
-    if (Kind_of_password == 2):
-        # Generate a password in which there are 
-        # no repeating letters (lowercase or uppercase) and numbers and punctations
-        # And cutted it to the selected length 
-        password_kind_input.password = "".join(random.sample((string.ascii_uppercase + string.ascii_lowercase + string.punctuation + string.digits),password_length.length_of_password))
-
-    # If selection is letters, punctuations
-    elif (Kind_of_password == 3):
+    # If selection is hexade
+    elif (password_kind_input.Kind_of_password == "2"):
         # Generate a password in which there are
         # no repeating numbers and punctations
         # And cutted it to the selected length
-        password_kind_input.password = "".join(random.sample((string.uppercase + string.ascii_lowercase + string.punctuation),password_length.length_of_password))
+        password_kind_input.password = "".join(random.sample(string.hexdigits,password_length.length_of_password))
+
+    # If selection is letters, punctuations
+    elif (password_kind_input.Kind_of_password == "3"):
+        # Generate a password in which there are
+        # no repeating numbers and punctations
+        # And cutted it to the selected length
+        password_kind_input.password = "".join(random.sample((string.ascii_uppercase + string.ascii_lowercase + string.punctuation),password_length.length_of_password))
 
     # If selection is letters, numbers
-    elif (Kind_of_password == 4):
+    elif (password_kind_input.Kind_of_password == "4"):
         # Generate a password in which there are
         # no repeating letters (lowercase or uppercase) and numbers
         # And cutted it to the selected length
         password_kind_input.password = "".join(random.sample((string.digits + string.ascii_lowercase + string.ascii_uppercase),password_length.length_of_password))
 
     # If selection is numbers, punctuations
-    elif (Kind_of_password == 5):
+    elif (password_kind_input.Kind_of_password == "5"):
         # Generate a password in which there are
         # no repeating numbers and punctations
         # And cutted it to the selected length
         password_kind_input.password = "".join(random.sample((string.digits + string.punctuation),password_length.length_of_password))
 
-    # If selection is only numbers
-    elif (Kind_of_password == 6):
-        # Generate a password in which there are
-        # no repeating numbers
-        # And cutted it to the selected length        
-        password_kind_input.password = "".join(random.sample(string.digits,password_length.length_of_password))
-
-    # If selection is only letters
-    elif (Kind_of_password == 7):
-        # Generate a password in which there are
-        # no repeating letters (lowercase or uppercase)
-        # And cutted it to the selected length
-        password_kind_input.password = "".join(random.sample((string.ascii_lowercase + string.ascii_uppercase),password_length.length_of_password))
-
-    # If selection is only punctuations    
-    elif (Kind_of_password == 8):
-        # Generate a password in which there are
-        # no repeating punctuations
-        # And cutted it to the selected length        
-        password_kind_input.password = "".join(random.sample(string.punctuation,password_length.length_of_password))
-
     # If the user selection is an invalid value
     else:
         # Start invalid function
         invalid_value()
-        if (user_selected == "Y" or user_selected == "y"):
-            ini()
-        elif (user_selected == "N" or user_selected == "n"):
+        if (invalid_value.user_selected == "Y" or invalid_value.user_selected == "y"):
+            password_kind_input()
+        elif (invalid_value.user_selected == "N" or invalid_value.user_selected == "n"):
             print()
         # If User selected invalid value
         else:
